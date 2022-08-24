8a5a836c11f97fb8a48f839abdfbfdebc04ec94c
Anantha5 <panantharamireddy111@gmail.com>
2022-08-24 22:06:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-wn2gkk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascript-wn2gkk)
---
+++ index.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 100; i++)
+ {
+  if (i % 3 == 0 && i % 5 == 0) console.log('amazon');
+  if (i % 3 == 0) console.log('google');
+  else if (i % 5 == 0) console.log('facebook');
+  else console.log(i);
+}
